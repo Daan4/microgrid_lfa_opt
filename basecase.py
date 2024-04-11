d3496b639908342d61b1d069c59a5eaf85fcf5ed
Daan <1859384+Daan4@users.noreply.github.com>
2024-04-11 16:04:38 +0200

    add function to load, resample and plot the data

--- basecase.py
+++ basecase.py
@@ -8,12 +8,39 @@ import matplotlib.pyplot as plt
 network = pypsa.Network()
 
 
-def initialize_network():
-    index = pd.date_range("2021-02-01 00:30", "2021-02-21 19:00", freq="30T")
-    data = pd.read_csv("data/load.csv")
+def get_load_data():
+    """
+    Get the load data from the csv and resample it from 30 min data to hourly data.
 
-    load_p_data = pd.Series(data.kW.values, index)
-    load_q_data = pd.Series(data.kVAr.values, index)
+    :return: Returns a tuple with the series index, active power series, and reactive power series
+    """
+
+    index = pd.date_range("2021-01-01 00:00", "2021-12-31 23:30", freq="30min")
+    data = pd.read_csv("data/load_half_hourly.csv")
+
+    load_p_data = pd.Series(data.kW.values, index).resample("1h").mean()
+    load_q_data = pd.Series(data.kVAr.values, index).resample("1h").mean()
+
+    return load_p_data.index, load_p_data, load_q_data
+
+
+def plot_load_data():
+    (index, load_p_data, load_q_data) = get_load_data()
+
+    plt.figure()
+    plt.plot(load_p_data, label="Active Power Demand")
+    plt.plot(load_q_data, label="Reactive Power Demand")
+    plt.plot(load_p_data+load_q_data, label="Apparent Power Demand")
+    plt.xlabel("Time")
+    plt.ylabel("Power [W/VAr/VA]")
+    plt.legend(loc="best")
+    plt.grid(True)
+    plt.title("Load Power Demand")
+    plt.show()
+
+
+def initialize_network():
+    (index, load_p_data, load_q_data) = get_load_data()
 
     network.snapshots = index
 
@@ -48,22 +75,23 @@ def calculate_diesel_setpoints(load_p_data, load_q_data):
 
 
 if __name__ == "__main__":
-    network = initialize_network()
-
-    network.determine_network_topology()
-    subnetworks = network.sub_networks
-
-    network.pf()
-
-    gen_p = network.generators_t.p
-    gen_q = network.generators_t.q
-
-    load_p = network.loads_t.p
-    load_q = network.loads_t.q
-
-    links_p = network.links_t.p0
-
-    bus_vmag = network.buses_t.v_mag_pu
-    bus_vang = network.buses_t.v_ang * 180 / np.pi
+    plot_load_data()
+    # network = initialize_network()
+    #
+    # network.determine_network_topology()
+    # subnetworks = network.sub_networks
+    #
+    # network.pf()
+    #
+    # gen_p = network.generators_t.p
+    # gen_q = network.generators_t.q
+    #
+    # load_p = network.loads_t.p
+    # load_q = network.loads_t.q
+    #
+    # links_p = network.links_t.p0
+    #
+    # bus_vmag = network.buses_t.v_mag_pu
+    # bus_vang = network.buses_t.v_ang * 180 / np.pi
 
     pass
